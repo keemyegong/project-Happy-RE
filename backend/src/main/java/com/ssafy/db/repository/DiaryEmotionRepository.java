ce2ba4a95ae0e6f03da804b3b5184a5aca586c4e
changhyeon12333 <96161882+changhyeon12333@users.noreply.github.com>
2024-07-18 16:38:25 +0900

    Token Done

--- backend/src/main/java/com/ssafy/db/repository/DiaryEmotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.db.repository;
-
-import com.ssafy.db.entity.DiaryEmotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiaryEmotionRepository extends JpaRepository<DiaryEmotion, Integer> {
-}
--- backend/src/main/java/com/ssafy/db/repository/KeywordEmotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.db.repository;
-
-import com.ssafy.db.entity.KeywordEmotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KeywordEmotionRepository extends JpaRepository<KeywordEmotion, Integer> {
-}
