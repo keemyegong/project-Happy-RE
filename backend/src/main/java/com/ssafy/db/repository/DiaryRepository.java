acf43a23c675598680fc6ac40a5b7a07e82135a8
YewoonKim <keemyegong@gmail.com>
2024-07-22 15:01:51 +0900

    first commit

--- backend/src/main/java/com/ssafy/db/repository/DiaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.db.repository;
-
-import com.ssafy.db.entity.Diary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiaryRepository extends JpaRepository<Diary, Integer> {
-}
