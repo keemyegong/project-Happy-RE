cd0cf2074c69576591a827bb5afc7aefc0166251
YewoonKim <keemyegong@gmail.com>
2024-07-22 17:10:19 +0900

    front structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/main/Main';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Happy:RE</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
