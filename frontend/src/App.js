9b8bff92e9bb6e04c51295d77a768891c08bbc10
pkm-master <tape3276@gmail.com>
2024-07-23 16:49:56 +0900

    fix:폴더 디렉터리 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './components/navbar/Navbar';
 import Main from './pages/main/Main';
+import Login from './pages/login/Login';
+import './App.css'
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Nav />
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
