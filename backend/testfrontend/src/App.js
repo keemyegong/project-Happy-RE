bc9a8fffa697421ead603bcf827d468bfa2c51d4
changhyeon12333 <96161882+changhyeon12333@users.noreply.github.com>
2024-07-23 17:45:47 +0900

    back_testfront

---
+++ backend/testfrontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FileUpload from './FileUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>S3 파일 업로드</h1>
+      </header>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
