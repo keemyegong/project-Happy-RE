b0613f2f3b8610a2f202da76226e8a5613a7e04e
unknown <gumtteun@gmail.com>
2024-07-24 14:33:48 +0900

    degine :  background star

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/navbar/Navbar';
 import Main from './pages/main/Main';
 import Login from './pages/login/Login';
-import './App.css'
+import UserTest from './pages/user/UserTest';
+import StarryBackground from './components/starry-background/StarryBackground';
+import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="App">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <StarryBackground />
+        <div className="content">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/usertest" element={<UserTest />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
