e3a32256dd5cc4b4a14202b66cafc326d8b0cb50
unknown <gumtteun@gmail.com>
2024-07-25 17:43:26 +0900

    design : MessageCard 제작 (최봉준)

--- frontend/src/components/emotion-graph/Test.js
+++ frontend/src/components/emotion-graph/Test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import EmotionGraph from './EmotionGraph';
 import './test.css';
 
+
+// sampleData 연결해서 x,y 좌표 받기
 const sampleData = [
   { x: -0.6, y: -0.6, value: 0.8 }, // 점 좌표와 세기값 세기값은 0.8 고정
   { x: -0.5, y: -0.5, value: 0.8 },
