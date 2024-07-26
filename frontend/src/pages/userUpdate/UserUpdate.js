f74a0420ae5cceae1e6c172553369e87b0ab1199
pkm-master <tape3276@gmail.com>
2024-07-26 14:05:32 +0900

    feature:유저 프로필 이미지 변경기능-현재 프론트만 작동 (김수민)

--- frontend/src/pages/userUpdate/UserUpdate.js
+++ frontend/src/pages/userUpdate/UserUpdate.js
@@ -1,4 +1,4 @@
-import React, { useState,useContext, useEffect } from 'react';
+import React, { useState,useContext, useEffect, useRef } from 'react';
 import {universeVariable} from '../../App';
 
 import axios from 'axios';
@@ -10,16 +10,40 @@ import Cookies from 'js-cookie';
 import { useNavigate  } from "react-router-dom";
 
 const UserUpdate = ()=>{
+
+  const [image,setImage] = useState(userProfileImage);
+  const [imagefile,setImageFile] = useState();
+  const fileInput = useRef(null)
+
   const universal = useContext(universeVariable);
   let navigate = useNavigate ();
   const [nickname, setNickname] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
+  const formData = new FormData();
+
+  const onChange = (e) => {
+
+    if(e.target.files[0]){
+      setImageFile(e.target.files[0]);
+      
+    }else{ 
+        return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState === 2){
+        setImage(reader.result);        
+      }
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
 
   useEffect(()=>{
     axios.get(`${universal.defaultUrl}/api/user/me`,
       {headers: {Authorization : `Bearer ${Cookies.get('Authorization')}`}}
     ).then((Response)=>{
+      console.log(Response.data);
       setNickname(Response.data.name);
     })
   },[])
@@ -27,30 +51,46 @@ const UserUpdate = ()=>{
     <div className='user-update-container'>
       <h1 className='text-center text-white'>Profile</h1>
       <div className='user-avatar'>
-        <img className='profile-image' src={userProfileImage}/>
+        <img className='profile-image' src={image} onClick={()=>{
+          fileInput.current.click()
+        }}/>
+         <input 
+          type='file' 
+          style={{display:'none'}}
+          accept='image/jpg,impge/png,image/jpeg' 
+          name='profile_img'
+          onChange={onChange}
+          ref={fileInput}/>
       </div>
       <div className='user-update-form'>
         <UserInfoInput
         setNickname={setNickname} setPassword={setPassword} setPassword2={setPassword2} nickname={nickname}/>
       </div>
       <Button className='btn dark-btn middle mb-3' content='Update' onClick={()=>{
+        formData.append('name',nickname);
+        formData.append('password',password);
+        if (imagefile){
+          formData.append('profileUrl',imagefile);
+        }
+
         if(password!==password2){
           alert('비밀번호가 다릅니다!')
         } else{
           axios.put(
             `${universal.defaultUrl}/api/user/me`,
-            {name:nickname,
-              password
-            },
-            {headers: {Authorization : `Bearer ${Cookies.get('Authorization')}`}}
+            formData,
+            {
+              headers: {'Content-Type': 'multipart/form-data',
+              Authorization : `Bearer ${Cookies.get('Authorization')}`}}
           ).then((Response)=>{
             console.log(Response.data);
             navigate('/profile');
-
           })
           .catch((err)=>{
             console.log(err);
-            
+            for (var key of formData.entries()) {
+              console.log(key[0] + ', ' + key[1]);
+          }
           })
         }
       }} />
