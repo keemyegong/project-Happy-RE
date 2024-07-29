bca77d6f9f1803c716c1c84661afee41f4380289
pkm-master <tape3276@gmail.com>
2024-07-29 16:51:33 +0900

    feature: 유저의 음성인식을 기반으로 gpt와 채팅할 수 있도록 하는 기능(김수민)

--- frontend/src/pages/aiChat/aiChat.js
+++ frontend/src/pages/aiChat/aiChat.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useContext } from "react";
+import React, { useState, useCallback,useEffect, useContext } from "react";
 import {universeVariable} from '../../App';
 import Cookies from 'js-cookie';
 import MicRecorder from 'mic-recorder-to-mp3';
@@ -8,13 +8,32 @@ import ChatCam from '../../components/ai-chat/ChatCam'
 import ChatBox from '../../components/ai-chat/ChatBox'
 
 const AIChat = () => {
-
+  const [userInputs,setUserInputs] = useState([]);
+  const [chatbotResponses,setChatbotResponses] = useState([]);
   const universal = useContext(universeVariable);
   const [number, setNumber] = useState(0);
   const [userInputText, setUserInputText ] = useState('');
+  const [chatbotResponse, setChatbotResponse] = useState('');
   const recorder = new MicRecorder({
     bitRate: 128
   });
+
+  // 들어가자마자 챗봇한테 처음 인삿말 받아오는 코드 - 개발중엔 꺼둘예정
+  // useEffect(()=>{
+  //   axios.post(
+  //     `${universal.fastUrl}/ai-api/chatbot/`,
+  //     {user_input:'안녕하세요'},
+  //     {headers: {
+  //       Authorization : `Bearer ${Cookies.get('Authorization')}`,
+  //       withCredentials: true,
+  //     }}
+  //     ).then((Response)=>{
+  //       setChatbotResponse(Response.data);
+  //       const tmpChatbotResponse =[...chatbotResponses];
+  //       tmpChatbotResponse.push(Response.data);
+  //       setChatbotResponses(tmpChatbotResponse);
+  //     })
+  // },[])
 
   // mp3 파일 제작용 레코드(녹음시작)
   const newRecord = ()=>{
@@ -55,8 +74,27 @@ const AIChat = () => {
         }}).then((Response)=>{
           console.log(Response.data.text);
           setUserInputText(Response.data.text);
+          const tmpUserInputs =[...userInputs];
+          tmpUserInputs.push(Response.data.text);
+          setUserInputs(tmpUserInputs);
 
-        }).catch((err)=>{
+          axios.post(
+          `${universal.fastUrl}/ai-api/chatbot/`,
+          {user_input:userInputText},
+          {headers: {
+            Authorization : `Bearer ${Cookies.get('Authorization')}`,
+            withCredentials: true,
+          }}
+          ).then((Response)=>{
+            console.log(Response.data);
+            setChatbotResponse(Response.data);
+            const tmpChatbotResponse =[...chatbotResponses];
+            tmpChatbotResponse.push(Response.data);
+            setChatbotResponses(tmpChatbotResponse);
+          })
+
+        })
+        .catch((err)=>{
           console.log(err);
         })
       })
@@ -64,15 +102,19 @@ const AIChat = () => {
 
   return (
     <div className='AIChat'>
-      <div className="AIChat-continaer">
-        <h1 className="text-white">AIChat</h1>
+      {/* 테스트용 컨테이너. */}
+      <div className="ai-chat-container">
         <h1 className="text-white">{number}</h1>
         <button onClick={newRecord}>녹음 시작</button>
         <button onClick={newRecordStop}>녹음 끝 + 전송</button>
         <p className="text-white">
-          {userInputText}
+          {userInputs}
+          ----
+          {chatbotResponses}
         </p>
       </div>
+
+
       <div className='ai-chat-container'>
         <ChatCam />
         <ChatBox />
