64616882a03f6c9097257aae2e2cb82819237e55
pkm-master <tape3276@gmail.com>
2024-07-31 16:57:30 +0900

    bugfix:RTC 서버 롤백(김수민)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const WebSocket = require('ws');
 const path = require('path');
