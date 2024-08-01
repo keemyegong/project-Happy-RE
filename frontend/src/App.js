84e31f09cb739d7d1e73bf9a9642ff9ce7eb0a52
pkm-master <tape3276@gmail.com>
2024-08-01 11:52:24 +0900

    bugfix:api url 오류 수정(김수민)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,7 +58,7 @@ const App = () => {
   return (
     <universeVariable.Provider
       value={{
-        defaultUrl: 'https://i11b204.p.ssafy.io/',
+        defaultUrl: 'https://i11b204.p.ssafy.io',
         fastUrl: 'http://192.168.31.229:8000',
       }}
     >
