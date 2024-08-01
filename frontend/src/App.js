1684fde11f6f605823d50fa8fb25eebd2c644af0
unknown <gumtteun@gmail.com>
2024-08-01 10:00:45 +0900

    fix : App.js navigate 수정 중 (최봉준)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,14 @@ const PrivateRoute = ({ children }) => {
   return children;
 };
 
+const PublicRoute = ({ children }) => {
+  const token = Cookies.get('Authorization');
+  if (token) {
+    return <Navigate to="/profile" />;
+  }
+  return children;
+};
+
 const AppContent = () => {
   const location = useLocation();
 
@@ -48,10 +56,10 @@ const AppContent = () => {
       <Nav />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signup/agreement" element={<SignUpAgreement />} />
+          <Route path="/" element={<PublicRoute><Main /></PublicRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/signup" element={<PublicRoute><SignUp /></PublicRoute>} />
+          <Route path="/signup/agreement" element={<PublicRoute><SignUpAgreement /></PublicRoute>} />
           <Route path="/emotion" element={<EmotionGraph />} />
           
           <Route path="/usertest" element={<PrivateRoute><UserTest /></PrivateRoute>} />
