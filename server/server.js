28059d6c61813e5245701507f6da071894e48e58
unknown <gumtteun@gmail.com>
2024-08-05 16:51:16 +0900

    fix : rtc 수정중(최봉준)

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ let users = {};
 let kurentoClient = null;
 let pendingConnections = [];
 
-const kurentoUri = 'ws://localhost:8888/kurento';
+const kurentoUri = 'ws://172.17.0.2:8888/kurento';  // Kurento Media Server의 IP 주소 사용
 
 const initializeKurentoClient = (retries = 0) => {
   kurento(kurentoUri, (error, client) => {
@@ -224,4 +224,4 @@ wss.on('connection', (ws, req) => {
 
 server.listen(5001, () => {
   console.log('Server is running on port 5001');
-});//
+});
