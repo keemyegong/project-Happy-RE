fb3ef001b3a088d4b3187f3c6207126e35213701
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-08-05 10:54:22 +0900

    fix: 오타수정

--- backend/Fast_API/api/app.py
+++ backend/Fast_API/api/app.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.abspath(os.path.join(current_dir, '..'))
 # Amazon S3 클라이언트
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id = os.getenv("AWS_ACCEsS_KEY_ID"),
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name = os.getenv("AWS_REGION")
 )
