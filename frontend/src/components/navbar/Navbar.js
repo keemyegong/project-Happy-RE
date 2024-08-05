3ca7f33ffbe8afa2815133199435791990f289a4
pkm-master <tape3276@gmail.com>
2024-08-05 15:29:05 +0900

    feature:로그아웃 구현(김수민)

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -13,11 +13,20 @@ function Nav() {
   // 토글 기능
   const toggleNavbar = () => setIsOpen(!isOpen);
 
+  const logout = ()=>{
+    Cookies.remove('Authorization',{path:'/'})
+    universal.setIsAuthenticated(false);
+
+  }
   // 쿠키에서 JWT 토큰 확인
-  // useEffect(() => {
-  //   const token = Cookies.get('Authorization');
-  //   setIsAuthenticated(!!token);
-  // }, []);
+  useEffect(() => {
+    const token = Cookies.get('Authorization');
+    if (token == null){
+      universal.setIsAuthenticated(false);
+    } else{
+      universal.setIsAuthenticated(true);
+    }
+  }, []);
 
   return (
     <> 
@@ -50,7 +59,7 @@ function Nav() {
                   <Link className='navbar-menu navbar-menu-font' to='/message'>MESSAGE</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className='navbar-menu navbar-icon' to='/logout'>
+                  <Link className='navbar-menu navbar-icon' to='/' onClick={logout}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
                       <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
                       <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
