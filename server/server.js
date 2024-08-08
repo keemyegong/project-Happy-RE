6a9bc9de6349f9485a7587e48046e0b8adf6df19
unknown <gumtteun@gmail.com>
2024-08-08 15:59:42 +0900

    fix : rtc 수정 중 (최봉준)

--- server/server.js
+++ server/server.js
@@ -167,6 +167,6 @@ wss.on('connection', (ws, req) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log('Server is running on port 5000');
+server.listen(5001, () => {
+  console.log('Server is running on port 5001');
 });
