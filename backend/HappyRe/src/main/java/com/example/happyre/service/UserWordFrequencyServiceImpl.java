9f953cf0c2855873949fcc44743ebd917838078f
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-08-08 14:20:39 +0900

    fix : log

--- backend/HappyRe/src/main/java/com/example/happyre/service/UserWordFrequencyServiceImpl.java
+++ backend/HappyRe/src/main/java/com/example/happyre/service/UserWordFrequencyServiceImpl.java
@@ -21,9 +21,11 @@ public class UserWordFrequencyServiceImpl implements UserWordFrequencyService {
         sentence.forEach(s -> {
             String[] splitWords = s.split(" ");  // 띄어쓰기로 문자열 분리
             for (String word : splitWords) {
+                System.out.println("++"+word+"++");
                 // 특수문자 제거
                 word = word.replaceAll("[^a-zA-Z0-9]", "");
                 if (!word.isEmpty()) {
+                    System.out.println("ADDDDDDDDD");
                     words.add(word);
                 }
             }
