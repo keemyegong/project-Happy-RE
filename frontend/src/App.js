0fd3e29ee9c82e19fb4070be5c5db7e70729df69
pkm-master <tape3276@gmail.com>
2024-08-09 17:49:35 +0900

    bugfix:url 수정(김수민)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -95,13 +95,12 @@ const App = () => {
   return (
     <universeVariable.Provider
       value={{
-        // defaultUrl: 'https://i11b204.p.ssafy.io',
-        // fastUrl: '',
-        // fastUrl: 'https://i11b204.p.ssafy.io',
-        defaultUrl: 'http://192.168.31.216:8080',
+        defaultUrl: 'https://i11b204.p.ssafy.io',
+        fastUrl: 'https://i11b204.p.ssafy.io',
+        // defaultUrl: 'http://192.168.31.216:8080',
         // defaultUrl: 'http://192.168.31.48:8080',
         // fastUrl: 'https://i11b204.p.ssafy.io',
-        fastUrl: 'http://192.168.31.229:8000', 
+        // fastUrl: 'http://192.168.31.229:8000', 
         isAuthenticated,
         setIsAuthenticated,
         todayDone,
