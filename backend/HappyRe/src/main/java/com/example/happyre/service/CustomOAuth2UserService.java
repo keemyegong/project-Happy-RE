59f00ec56bf565d9830d396b1c48ff3dc54223c2
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-08-09 14:05:03 +0900

    log : google

--- backend/HappyRe/src/main/java/com/example/happyre/service/CustomOAuth2UserService.java
+++ backend/HappyRe/src/main/java/com/example/happyre/service/CustomOAuth2UserService.java
@@ -1,9 +1,6 @@
 package com.example.happyre.service;
 
-import com.example.happyre.dto.oauth.CustomOAuth2User;
-import com.example.happyre.dto.oauth.GoogleResponse;
-import com.example.happyre.dto.oauth.NaverResponse;
-import com.example.happyre.dto.oauth.OAuth2Response;
+import com.example.happyre.dto.oauth.*;
 import com.example.happyre.dto.user.UserDTO;
 import com.example.happyre.entity.UserEntity;
 import com.example.happyre.repository.UserRepository;
@@ -27,16 +24,32 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         System.out.println(oAuth2User);
-
+//        properties={nickname=이창현,
+//                profile_image=http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640,
+//                 thumbnail_image=http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R110x110},
+//                kakao_account={profile_nickname_needs_agreement=false,
+//                                profile_image_needs_agreement=false,
+//                                profile={nickname=이창현,
+//                                        thumbnail_image_url=http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R110x110,
+//                                        profile_image_url=http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640,
+//                                        is_default_image=true, is_default_nickname=false},
+//                                has_email=true,
+//                                email_needs_agreement=false,
+//                                is_email_valid=true,
+//                                is_email_verified=true,
+//                                email=ckd5508@naver.com}
+//        }
+//
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2Response oAuth2Response = null;
         if (registrationId.equals("naver")) {
             oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
         } else if (registrationId.equals("google")) {
+            System.out.println(oAuth2User.getAttributes());
             oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
         } else if (registrationId.equals("kakao")) {
             System.out.println(oAuth2User.getAttributes());
-            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+            oAuth2Response = new KaKaoResponse(oAuth2User.getAttributes());
         }else{
             return null;
         }
@@ -57,6 +70,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
             UserDTO userDTO = new UserDTO();
             userDTO.setUsername(username);
+            userDTO.setEmail(email);
             userDTO.setName(oAuth2Response.getName());
             userEntity.setRole("ROLE_USER");
 
