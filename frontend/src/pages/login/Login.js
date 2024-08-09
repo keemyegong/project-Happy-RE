9afaf95ed8c8a91c5aabfcdb663ab7a389ad7bf2
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-08-09 15:46:35 +0900

    img

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState,useContext } from 'react';
 import {universeVariable} from '../../App';
-import kakaoimg from '../../assets/images/7595660_kakao_message_social_logo_icon.png';
-import googleimg from '../../assets/images/imgbin_google-logo-business-png.png';
+import kakaoimg from '../../assets/kakaologo.png';
+import googleimg from '../../assets/googlelogo.png';
 import './Login.css';
 import { Link } from 'react-router-dom';
 import loginTitle from '../../assets/login_title.png'
@@ -23,7 +23,7 @@ function Login() {
     window.location.href = `${universal.defaultUrl}/api/oauth2/authorization/google`
     
   }
-  const naverLogin = ()=>{
+  const kakaoLogin = ()=>{
     window.location.href = `${universal.defaultUrl}/api/oauth2/authorization/kakao`
   }
 
@@ -62,14 +62,9 @@ function Login() {
           <div className='login-title'>
             <img width='200px' src={loginTitle}/>
           </div>
-          <svg xmlns="http://www.w3.org/2000/svg" width="0.98em" height="1em" viewBox="0 0 256 262"><path fill="#4285f4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"/><path fill="#34a853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"/><path fill="#fbbc05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"/><path fill="#eb4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"/></svg>
-          <img src={googleimg} alt="Google" style={{ width: '100px', height: '100px' }} />
+          <img src={googleimg} onClick={googleLogin}alt="Google" style={{ width: '100px', height: '100px' }} />
+          <img src={kakaoimg} onClick={kakaoLogin} alt="Kakao" style={{ width: '100px', height: '100px' }} />
 
-          <img src={kakaoimg} alt="Kakao" style={{ width: '100px', height: '100px' }} />
-          <button className='btn google-login-btn ms-0'
-          onClick={googleLogin}>Login With Google</button>
-          <button className='btn google-login-btn ms-0'
-          onClick={naverLogin}>Login With Kakao</button>
         </div>
         <hr className='border-light border-1' />
       
