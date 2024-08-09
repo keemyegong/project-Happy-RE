cef1004099442bc0e10288949829828fec937820
YewoonKim <keemyegong@gmail.com>
2024-08-09 16:40:46 +0900

    front:메시지디자인

--- frontend/src/components/message-card/MessageCard.js
+++ frontend/src/components/message-card/MessageCard.js
@@ -53,9 +53,12 @@ const MessageCard = ({ messageId, profileImageUrl, userName, content, keyword })
         <div className="user-info">
           <h3 className="user-name">{userName}</h3>
           <button className={`favorite-button ${favorite ? 'favorite' : ''}`} onClick={handleFavoriteClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-star-fill click-star ${favorite ? 'filled' : ''}`} viewBox="0 0 16 16">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" className={`bi bi-star-fill click-star ${favorite ? 'filled' : ''}`} viewBox="0 0 16 16">
               <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-            </svg>
+            </svg> */}
+              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="22" fill="currentColor" className={`bi bi-star-fill click-star ${favorite ? 'filled' : ''}`} viewBox="0 0 21 22">
+                <path d="M7.11642 2.97649C8.14299 -0.35375 12.857 -0.35375 13.8836 2.97649C14.3413 4.46125 15.7283 5.47417 17.2821 5.47417C20.7653 5.47417 22.3243 9.89313 19.5588 12.011L19.2566 12.2424C18.0329 13.1796 17.5219 14.7794 17.976 16.2524L18.1322 16.7592C19.1404 20.03 15.3542 22.6657 12.6368 20.5848C11.3759 19.6191 9.62409 19.6191 8.36317 20.5848C5.64582 22.6657 1.85956 20.03 2.8678 16.7592L3.02404 16.2524C3.47808 14.7794 2.96715 13.1796 1.74345 12.2424L1.44122 12.011C-1.32427 9.89313 0.23467 5.47417 3.71795 5.47417C5.27165 5.47417 6.65873 4.46125 7.11642 2.97649Z" />
+              </svg>
           </button>
         </div>
       </div>
@@ -67,7 +70,6 @@ const MessageCard = ({ messageId, profileImageUrl, userName, content, keyword })
       <div className='message-card-keyword-content'>
         {keyword != undefined && <KeywordCard props={keyword.keywordEntity} emotiontags={keyword.keywordEntity.
 keywordEmotionEntityList} />}
-        
       </div>
     </div>
   );
--- frontend/src/components/message-input/MessageInput.js
+++ frontend/src/components/message-input/MessageInput.js
@@ -76,21 +76,59 @@ const MessageInput = ({keywords}) => {
         <textarea
           className="text-input"
           placeholder=" 해피리에서는 따뜻한 대화를 나누는 게 중요해요 
-            메시지를 보내기 전,
-            받는 사람의 기분을 한 번 더 생각해 주세요"
+            메시지를 보내기 전, 받는 사람의 기분을 한 번 더 생각해 주세요
+            공유하고 싶은 오늘의 키워드를 선택할 수 있어요!
+            "
           value={message}
           onChange={handleInputChange}
         ></textarea>
         <div className='message-input-keyword'>
-          <div className='message-keyword-left-button'>
-            <button disabled={leftdisable} onClick={goLeftKeyword}>left</button>
-          </div>
+        <button
+          onClick={goLeftKeyword}
+          disabled={leftdisable}
+          className='message-input-keyword-left-btn'
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-chevron-compact-left"
+            viewBox="0 0 16 16"
+            style={{
+              cursor: leftdisable ? 'default' : 'pointer',
+              opacity: leftdisable ? 0.5 : 1, // 비활성화 시 투명도 조정
+            }}
+          >
+          <path
+            fillRule="evenodd"
+            d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223"
+          />
+            </svg>
+          </button>
           <div className='message-keyword-card'>
             { keywords[keywordId] !== undefined && <KeywordCard props={keywords[keywordId]} />}
           </div>
-          <div className='message-keyword-left-button'>
-            <button disabled={rightdisable} onClick={goRightKeyword}>right</button>
-          </div>
+          <button
+          onClick={goRightKeyword}
+          disabled={rightdisable}
+          className='message-input-keyword-left-btn'
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-chevron-compact-left"
+            viewBox="0 0 16 16"
+            style={{
+              cursor: rightdisable ? 'default' : 'pointer',
+              opacity: rightdisable ? 0.5 : 1, // 비활성화 시 투명도 조정
+            }}
+          >
+          <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671"/>
+            </svg>
+          </button>
         </div> 
       </div>
       <hr className="divider" />
