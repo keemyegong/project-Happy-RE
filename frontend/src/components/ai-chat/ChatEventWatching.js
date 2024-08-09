f87235aae7a43e35c46ce83a6e4ba55c4f154084
happyyongku <yonggu1951@gmail.com>
2024-08-09 10:51:43 +0900

    temp

--- frontend/src/components/ai-chat/ChatEventWatching.js
+++ frontend/src/components/ai-chat/ChatEventWatching.js
@@ -11,7 +11,7 @@ const textList = [
     "테스트 문장 3번"
 ]
 
-const ChatEventWatching = ({endChatSession})=>{
+const ChatEventWatching = ({endChatSession, onClose})=>{
     const [balls, setBalls] = useState([]);
     const [numberOfBall, setNumberOfBalls] = useState(Math.floor(Math.random() * 5) + 3);
     const [currentText, setCurrentText] = useState(textList[0]);
@@ -80,7 +80,10 @@ const ChatEventWatching = ({endChatSession})=>{
 
     return(
         <div className='bouncing-ball-container'>
-            <div className='col-3 chat-quit' onClick={endChatSession}>
+            <div className='col-3 chat-quit' onClick={() => {
+                onClose();
+                endChatSession();
+            }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
