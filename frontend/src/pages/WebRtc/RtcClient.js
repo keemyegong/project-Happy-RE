14302d0334400f959290e2d65d52db1164e96f45
unknown <gumtteun@gmail.com>
2024-08-09 15:18:35 +0900

    fix : rtc 수정중(최봉준)

--- frontend/src/pages/WebRtc/RtcClient.js
+++ frontend/src/pages/WebRtc/RtcClient.js
@@ -497,6 +497,7 @@ const RtcClient = ({ initialPosition, characterImage }) => {
     }
   };
 
+
   return (
 <div className="chat-room-container" ref={containerRef}>
   <div className="chat-graph-audio-container">
