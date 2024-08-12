04112d5c815486e84ebae910a027c3a52f1fc871
happyyongku <yonggu1951@gmail.com>
2024-08-12 17:34:49 +0900

    hotfix : 요약 시 동일 키워드 제한

--- backend/Fast_API/chatbot/app.py
+++ backend/Fast_API/chatbot/app.py
@@ -201,7 +201,7 @@ async def session_delete(request:Request):
     
     chatbot_instance = user_session[user_id]
     prompt = '지금까지의 대화에서 전체적인 대화를 요약할 수 있는 짧게 요약된 문장과 \
-        긍정적인 감정이 느껴지는 3개 이하의 키워드와 부정적인 감정이 느껴지는 3개 이하의 키워드를 뽑아 유저 메세지와 함께 보여줘.\
+        긍정적인 감정이 느껴지는 서로 다른 3개 이하의 키워드와 부정적인 감정이 느껴지는 서로 다른 3개 이하의 키워드를 뽑아 유저 메세지와 함께 보여줘.\
         이때 너에게 설정된 persona를 무시하고, 오로지 유저의 입력에서만 키워드를 생성, 응답해줘.\
         Tips on Creating Keywords: 1. The keyword must NOT be emotion or thought. 2. pick most triggering keywords.\
         응답 결과는 반드시 요약 결과만을 JSON 형태로 제공하는데, 이 딕셔너리는 키값으로 "diary_summary"와 "summary_detail"을 가져. \
