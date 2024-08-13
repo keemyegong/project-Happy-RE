6d5dd2dd15d78eb6298cd2787e321f037f7cfacf
myDdoDdomi <gumtteun@gmail.com>
2024-08-13 22:32:15 +0900

    fix: Rtc 메세지큐 생성 (최봉준)

--- frontend/src/pages/WebRtc/MessageQueue.js
+++ frontend/src/pages/WebRtc/MessageQueue.js
@@ -33,3 +33,68 @@ class MessageQueue {
   
   export default MessageQueue;
   
+  MessageQueue.prototype.handleMessage = function(message) {
+    return new Promise((resolve) => {
+      switch (message.type) {
+        case 'assign_id':
+          setClientId(message.id);
+          client.send(JSON.stringify({
+            type: 'connect',
+            position,
+            characterImage: userImage,
+          }));
+          break;
+        case 'update':
+          const filteredUsers = message.clients.map(user => ({
+            ...user,
+            position: user.position || { x: 0, y: 0 },
+            connectedAt: user.connectedAt || 0,
+          }));
+          setUsers(filteredUsers.filter(user => user.id !== clientId).map(user => ({
+            ...user,
+            image: user.characterImage,
+          })));
+          const currentUser = filteredUsers.find(user => user.id === clientId);
+          if (currentUser) {
+            setCoolTime(currentUser.coolTime);
+            setNearbyUsers((currentUser.connectedUsers || []).map(connectedUser => ({
+              id: connectedUser.id,
+              image: connectedUser.characterImage,
+            })));
+          } else {
+            setNearbyUsers([]);
+          }
+          break;
+        case 'offer':
+          handleOffer(message.offer, message.sender);
+          break;
+        case 'answer':
+          handleAnswer(message.answer, message.sender);
+          break;
+        case 'candidate':
+          handleCandidate(message.candidate, message.sender);
+          break;
+        case 'rtc_disconnect':
+          handleRtcDisconnect(message.targetId);
+          break;
+        case 'talking':
+          setTalkingUsers(message.talkingUsers);
+          break;
+        case 'start_webrtc':
+          if (message.role === 'offer') {
+            const peerConnection = createPeerConnection(message.targetId);
+            attemptOffer(peerConnection, message.targetId);
+          } else if (message.role === 'answer') {
+            const peerConnection = createPeerConnection(message.targetId);
+            setPeerConnections(prevConnections => ({
+              ...prevConnections,
+              [message.targetId]: { peerConnection },
+            }));
+          }
+          break;
+        default:
+          console.error('Unrecognized message type:', message.type);
+      }
+      resolve();
+    });
+  };
