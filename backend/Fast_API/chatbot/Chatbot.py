a17a3af5079c96a52ecaa002a5e77c96c2c38b4d
happyyongku <yonggu1951@gmail.com>
2024-08-13 10:15:42 +0900

    temp

--- backend/Fast_API/chatbot/Chatbot.py
+++ backend/Fast_API/chatbot/Chatbot.py
@@ -68,28 +68,13 @@ class SentenceDividingChatbot:
         promptTemplate=[
             (
                 "system",
-                "You are a cute sentient jellyfish named {pname}.\
-                you must talk in KOREAN.\
-                here's some infos definning your personality. {pdescribe}\
-                and you are {consult_style}\
-                you must speak in the same tone and manner as the following examples, but not exactly the same.{examples}\
-                you must respond in a {style}\
-                If the examles use polite speech, you should only respond in polite speech,\
-                and if the example use informal speech, you should respond in informal speech.\
-                then, you take persona of {pname}\
-                response should be less than three sentences\
-                consistency is crucial. e.g) you must maintain informal/polite speech throughout the conversation, not alternating both\
-                you will be penalized or rewarded with $1000 according to your performance",
+                ""
             ),
             MessagesPlaceholder(variable_name="messages"),
         ],
-        persona={
-            "pname":"happy:RE"
-        },
     ):
         self.llm = ChatOpenAI(model=model, temperature=temperature)
         self.promptTemplate = promptTemplate
-        self.persona = persona
         self.history = ChatMessageHistory()
         prompt = ChatPromptTemplate.from_messages(
             self.promptTemplate,
@@ -102,7 +87,6 @@ class SentenceDividingChatbot:
 
         response = self.chain.invoke(
             {
-                **self.persona,
                 "messages": self.history.messages,
             }
         )
